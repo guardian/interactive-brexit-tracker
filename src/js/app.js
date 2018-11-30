dfe217e898a234318e1e8255db475a20d5297977
Antonio Voce <antonio.voce@guardian.co.uk>
2018-11-30 10:24:30 +0000

    Set up client-side react

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,14 @@
-// javascript goes here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import axios from 'axios'
+
+async function render() {
+  // const resp = await axios.get(`https://interactive.guim.co.uk/docsdata-test/1RPOsSCSg6lz42ToQ2zi9JE2rB9cI1ls4-njSYxda7fY.json`)
+  ReactDOM.render(
+    <App />,
+    document.getElementById("interactive-wrapper")
+  );
+}
+
+render()
