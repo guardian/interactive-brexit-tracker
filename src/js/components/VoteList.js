e24b303ee7f224f494c03ebff150c212d1c09dfa
Seán Clarke <sean.clarke@guardian.co.uk>
2018-12-03 13:32:08 +0000

    now with expandable details per mp

---
+++ src/js/components/VoteList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+function prettyVoteName (vote) {
+	if (vote == "AyeVote") {return "For"} 
+	else if (vote == "NoVote") {return "Against"}
+	else {return "Did not vote"} 
+}
+
+
+class Vote extends Component {
+    constructor (props ) {
+        super(props)
+    }
+
+    render() {
+
+        const vote = this.props.vote;
+        return <div>{vote.divisionNumber} {vote.prettyvote}</div>
+    }
+}
+
+
+export default class VoteList extends Component {
+    constructor(props) {
+      super(props)
+    }
+  
+    render() {
+        const votes = this.props.votes;
+        var voteListItems = []
+          votes.forEach(v => {
+           var voteItem = <Vote vote={v} key={v.divisionNumber}/>
+           voteListItems.push(voteItem);
+       })
+
+
+      return (
+          <div>{voteListItems}</div>
+      )
+    }
+  }
