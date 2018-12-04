3381420d36c18f79110bae35d72f25ead5b09f65
Seán Clarke <sean.clarke@guardian.co.uk>
2018-12-04 08:03:52 +0000

    table now sortable

--- src/js/components/Table.js
+++ src/js/components/Table.js
@@ -3,15 +3,29 @@ import Search from './Search.js'
 import ErrorBoundary from './ErrorBoundary.js'
 import TableList from './TableList.js'
 
+
+
+
+
 export default class Table extends Component {
     constructor(props) {
       super(props);
       this.state = {
         filterText: '',
-        sortConditions: {}
+        sortConditions: {
+        }
       }
       this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
-  
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e,column) {
+      var newState = Object.assign({},this.state);
+      newState.sortConditions.column = column;
+      if (newState.sortConditions.direction == 'up') {
+        newState.sortConditions.direction = 'down'
+      } else {newState.sortConditions.direction = 'up'}
+      this.setState(newState);
     }
 
     handleFilterTextChange(filterText) {
@@ -31,7 +45,15 @@ export default class Table extends Component {
         <div className='gv-search'>
         <Search filterText={this.state.filterText}   onFilterTextChange={this.handleFilterTextChange}/>
         </div>
-        <TableList members={membersInfo} filterText={this.state.filterText}/>
+        <div className="gv-table-header-group">
+      <div className="gv-table-header gv-name" onClick={e => this.handleClick(e,'listAs')}>Name</div>
+      <div className="gv-table-header gv-party" onClick={e => this.handleClick(e,'party')}>Party</div>
+      <div className="gv-table-header gv-constituency" onClick={e => this.handleClick(e,'constituency')}>Constituency</div>
+      <div className="gv-table-header gv-vote">Main vote</div>
+
+      </div>
+
+        <TableList members={membersInfo} filterText={this.state.filterText} sortConditions={this.state.sortConditions}/>
         </div>
       );
     
