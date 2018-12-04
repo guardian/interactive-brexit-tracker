38fd6878aceea12811cc1f610a69fb20b87c91f6
Antonio Voce <antonio.voce@guardian.co.uk>
2018-12-04 15:34:39 +0000

    Paths done

--- src/js/components/Waffle.js
+++ src/js/components/Waffle.js
@@ -14,10 +14,13 @@ class Waffle extends Component {
     const noes = members.filter(d => d.vote === 'NoVote').length
     const abstained = members.filter(d => d.vote === 'A').length
     const rows = 13;
-    const absDiv = Math.round(abstained / 13)
+    
     const absOdd = rows - (abstained % 13)
     const ayesOdd = ayes % 13
     const noesOdd = noes % 13
+
+    const absDiv = noesOdd !== 0 ? Math.ceil(abstained / 13) + 1 : Math.ceil(abstained / 13)
+
     const columns = 50;
     const width = 860;
     const totalMps = 649
@@ -27,14 +30,16 @@ class Waffle extends Component {
     const addedWidth = width - (50 * sqWidth)
     const positions = generatePositions(columns, rows, sqWidth, sqHeight)
 
-    console.log(absOdd)
 
     const noPath = ayesOdd !== 0 ?
-      `M ${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2} L${positions[ayes][0] + sqWidth -2 } ${sqWidth * ayesOdd - 2} L${positions[ayes][0] + sqWidth -2 } -2 L${8 + width - absDiv * sqWidth} -2 L${8 + width - absDiv * sqWidth} ${ absOdd !== 0 ? 2 + absOdd * sqWidth : height + 2} L${absOdd !== 0 ? width - 8 -absDiv * sqWidth : width + 2} ${absOdd * sqWidth + 2} L${absOdd !== 0 ? width - 8 -absDiv * sqWidth : width + 2} ${height + 2}`:
-      ``
+      `M ${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2} L${positions[ayes][0] + sqWidth -2 } ${sqWidth * ayesOdd - 2} L${positions[ayes][0] + sqWidth -2 } -2 L${8 + width - absDiv * sqWidth} -2 L${8 + width - absDiv * sqWidth} ${ absOdd !== 0 ? 1 + absOdd * sqWidth : height + 1} L${absOdd !== 0 ? width - 9 -absDiv * sqWidth : width + 2} ${absOdd * sqWidth + 1} L${absOdd !== 0 ? width - 9 -absDiv * sqWidth : width + 2} ${height + 1} L${positions[ayes][0] - 2} ${height + 1} L${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2}`:
+      `M ${positions[ayes][0] - 2} ${height + 1.5} L${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2} L${8+width - absDiv * sqWidth} -2 L${8+width - absDiv * sqWidth} ${absOdd !== 0 ? absOdd * sqWidth + 1 : height + 1} L${absOdd !== 0 ? width - 9 - absDiv * sqWidth : width + 2} ${absOdd !== 0 ? 1+ absOdd * sqWidth : height + 1} L${absOdd !== 0 ? width - 9 - absDiv * sqWidth : width + 2} ${height + 1} L${positions[ayes][0] - 2} ${height +1} `
+
     const ayePath = ayesOdd !== 0 ?
       `M -2 -2 L${positions[ayes][0] + sqWidth + 1} -2 L${positions[ayes][0] + sqWidth + 1} ${sqWidth * ayesOdd + 1} L${positions[ayes][0] + 1} ${sqWidth * ayesOdd + 1} L${positions[ayes][0] + 1} ${height +1} L -2 ${height +1} L -2 -2.7` :
       `M -2 -2 L${positions[ayes][0] + 1} -2 L${positions[ayes][0] +1} ${height + 1} L -2 ${height + 1} L -2 -2.7`
+
+    // const winnerPath
 
     return (
       <div>
@@ -52,8 +57,9 @@ class Waffle extends Component {
           }
           </g>
           {/*<line x1={positions[votingMps * 0.5 - 1][0]} x2={positions[votingMps * 0.5 - 1][0]} y1="-20" y2={height + 20} stroke="#999999" fill="none" strokeWidth="2px"></line>*/}
-          {/*<path d={ayePath} stroke="#000" strokeWidth="2px" fill="none" />*/}
-          <path d={noPath} stroke="#000" strokeWidth="2px" fill="none" />
+          <path d={winnerPath} />
+          {ayes >= noes && <path d={ayePath} stroke="#000" strokeWidth="3px" fill="none" />}
+          {noes >= ayes && <path d={noPath} stroke="#000" strokeWidth="3px" fill="none" />}
         </svg>
       </div>
     )
