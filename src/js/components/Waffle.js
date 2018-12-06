33cfed6f6f263f48a6071d5de94992312f206dfc
Niko Kommenda <nkommenda@hotmail.com>
2018-12-06 16:50:59 +0000

    width fix ...phew

--- src/js/components/Waffle.js
+++ src/js/components/Waffle.js
@@ -25,12 +25,14 @@ class Waffle extends Component {
     const width = 860;
     const totalMps = 649
     const sqWidth = Math.round(width / columns);
-    const sqHeight = Math.round(sqWidth);
+    const sqHeight = sqWidth
     const height = sqHeight * rows;
     const addedWidth = width - (50 * sqWidth)
     const positions = generatePositions(columns, rows, sqWidth, sqHeight)
 
     const needed = Math.ceil(votingMps/2)
+
+    const ayesWin = ayes > noes
 
     console.log(votingMps, 'voting')
     console.log(ayes, 'ayes')
@@ -41,12 +43,12 @@ class Waffle extends Component {
     console.log(ayesOdd, 'ayes modulo')
 
     const noPath = ayesOdd !== 0 ?
-      `M ${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2} L${positions[ayes][0] + sqWidth -2 } ${sqWidth * ayesOdd - 2} L${positions[ayes][0] + sqWidth -2 } -2 L${8 + width - absDiv * sqWidth} -2 L${8 + width - absDiv * sqWidth} ${ absOdd !== 0 ? 1 + absOdd * sqWidth : height + 1} L${absOdd !== 0 ? width - 9 -absDiv * sqWidth : width + 2} ${absOdd * sqWidth + 1} L${absOdd !== 0 ? width - 9 -absDiv * sqWidth : width + 2} ${height + 1} L${positions[ayes][0] - 2} ${height + 1} L${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2}`:
-      `M ${positions[ayes][0] - 2} ${height + 1.5} L${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2} L${8+width - absDiv * sqWidth} -2 L${8+width - absDiv * sqWidth} ${absOdd !== 0 ? absOdd * sqWidth + 1 : height + 1} L${absOdd !== 0 ? width - 9 - absDiv * sqWidth : width + 2} ${absOdd !== 0 ? 1+ absOdd * sqWidth : height + 1} L${absOdd !== 0 ? width - 9 - absDiv * sqWidth : width + 2} ${height + 1} L${positions[ayes][0] - 2} ${height +1} `
+      `M ${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2} L${positions[ayes][0] + sqWidth -2 } ${sqWidth * ayesOdd - 2} L${positions[ayes][0] + sqWidth -2 } -2 L${8 + width - absDiv * sqWidth} -2 L${8 + width - absDiv * sqWidth} ${ absOdd !== 0 ? 1 + absOdd * sqWidth : height + 1} L${absOdd !== 0 ? width - 9 -absDiv * sqWidth : width + 2} ${absOdd * sqWidth + 1} L${absOdd !== 0 ? width - 9 -absDiv * sqWidth : width + 2} ${height + 1} L${positions[ayes][0] - 2} ${height + 1} L${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2} Z`:
+      `M ${positions[ayes][0] - 2} ${height + 1.5} L${positions[ayes][0] - 2} ${sqWidth * ayesOdd - 2} L${8+width - absDiv * sqWidth} -2 L${8+width - absDiv * sqWidth} ${absOdd !== 0 ? absOdd * sqWidth + 1 : height + 1} L${absOdd !== 0 ? width - 9 - absDiv * sqWidth : width + 2} ${absOdd !== 0 ? 1+ absOdd * sqWidth : height + 1} L${absOdd !== 0 ? width - 9 - absDiv * sqWidth : width + 2} ${height + 1} L${positions[ayes][0] - 2} ${height +1} Z`
 
     const ayePath = ayesOdd !== 0 ?
-      `M -2 -2 L${positions[ayes][0] + sqWidth + 1} -2 L${positions[ayes][0] + sqWidth + 1} ${sqWidth * ayesOdd + 1} L${positions[ayes][0] + 1} ${sqWidth * ayesOdd + 1} L${positions[ayes][0] + 1} ${height +1} L -2 ${height +1} L -2 -2.7` :
-      `M -2 -2 L${positions[ayes][0] + 1} -2 L${positions[ayes][0] +1} ${height + 1} L -2 ${height + 1} L -2 -2.7`
+      `M -2 -2 L${positions[ayes][0] + sqWidth + 1} -2 L${positions[ayes][0] + sqWidth + 1} ${sqWidth * ayesOdd + 1} L${positions[ayes][0] + 1} ${sqWidth * ayesOdd + 1} L${positions[ayes][0] + 1} ${height +1} L -2 ${height +1} L -2 -2 Z` :
+      `M -2 -2 L${positions[ayes][0] + 1} -2 L${positions[ayes][0] +1} ${height + 1} L -2 ${height + 1} L -2 -2 Z`
 
     const winnerPath = absModulo === 0 ? `M ${positions[Math.floor(votingMps * 0.5)][0] + sqWidth} -20 L ${positions[Math.floor(votingMps * 0.5)][0] + sqWidth} ${height + 20}` :
       // `M ${positions[Math.floor(votingMps * 0.5)][0] + sqWidth} -20 L${positions[Math.floor(votingMps * 0.5)][0] + sqWidth} ${Math.floor(absModulo * 0.5) * sqWidth} L${positions[Math.floor(votingMps * 0.5)][0]} ${Math.floor(absModulo * 0.5) * sqWidth} L${positions[Math.floor(votingMps * 0.5)][0]} ${height + 20}`
@@ -57,29 +59,41 @@ class Waffle extends Component {
     const rest = needed % 13
 
     const neededPoints = [
+      [ (neededCols + (rest ? 1 : 0))*sqWidth, -20 ],
       [ (neededCols + (rest ? 1 : 0))*sqWidth, 0 ],
       [ ( neededCols + (rest ? 1 : 0))*sqWidth, rest*sqHeight ],
       [ neededCols*sqWidth, rest*sqHeight ],
-      [ neededCols*sqWidth, 13*sqHeight ]
+      [ neededCols*sqWidth, 13*sqHeight ],
+      [ neededCols*sqWidth, 13*sqHeight + 20 ]
     ]
     
     const neededPath = `M ${neededPoints[0].join(',')} ` + ' L' + neededPoints.slice(1).map( p => p.join(',') ).join(' L')
     
     console.log(neededPath)
 
+    const noesStyle = {
+      right: (100 - Math.ceil(votingMps/13)/50*100) + '%'
+    }
+
+    console.log(noesStyle)
+
     return (
-      <div>
-        <svg style={{overflow: 'visible'}} viewBox={`0 0 ${width} ${height}`} xmlns="http://www.w3.org/2000/svg">
+      <div className='gv-waffle-container'>
+
+        <h2 className='gv-count gv-count--ayes'>{ayes}</h2>
+        <h2 className='gv-count gv-count--noes' style={noesStyle}>{noes}</h2>
+
+        <svg style={{overflow: 'visible'}} viewBox={`0 0 ${849} ${height + 50}`} xmlns="http://www.w3.org/2000/svg">
           <g>
           {
             sortByOccurrence(members.filter(d => d.vote === 'AyeVote'), 'aye')
-              .map((d, i) => <rect key={d.id} id={'aye-' + d.id} height={sqHeight - 1} width={sqWidth - 1} x={positions[i][0]} y={positions[i][1]} fill={partyColours[d.party]}></rect>)
+              .map((d, i) => <rect key={d.id} id={'aye-' + d.id} height={sqHeight - 1} width={sqWidth - 1} x={positions[i][0]} y={positions[i][1]} fill={partyColours[d.party]} fillOpacity={ ayesWin ? 1 : 0.6 }></rect>)
           }
           </g>
           <g>
           {
             sortByOccurrence(members.filter(d => d.vote === 'NoVote'), 'no')
-                .map((d, i) => <rect key={d.id} id={'no-' + d.id} height={sqHeight - 1} width={sqWidth - 1} x={positions[ayes + i][0]} y={positions[ayes + i][1]} fill={partyColours[d.party]}></rect>)
+                .map((d, i) => <rect key={d.id} id={'no-' + d.id} height={sqHeight - 1} width={sqWidth - 1} x={positions[ayes + i][0]} y={positions[ayes + i][1]} fill={partyColours[d.party]} fillOpacity={ ayesWin ? 0.6 : 1 }></rect>)
           }
           </g>
           {/*<line x1={positions[votingMps * 0.5 - 1][0]} x2={positions[votingMps * 0.5 - 1][0]} y1="-20" y2={height + 20} stroke="#999999" fill="none" strokeWidth="2px"></line>*/}
