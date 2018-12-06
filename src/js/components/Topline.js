f28b215a590e55f48f6864175ace5ca286e53410
Antonio Voce <antonio.voce@guardian.co.uk>
2018-12-06 19:06:47 +0000

    Fix for missing amendments

--- src/js/components/Topline.js
+++ src/js/components/Topline.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { partyColours } from '../util'
 
 const Topline = ({ divInfo }) => {
+  console.log(divInfo)
   const votingMps = divInfo.ayesCount + divInfo.noesCount
   const sides = divInfo.ayesCount >= divInfo.noesCount ?
     [{ key: 'Ayes', val: divInfo.ayesByParty }, { key: 'Noes', val: divInfo.noesByParty }] :
@@ -10,7 +11,8 @@ const Topline = ({ divInfo }) => {
   return (
     <div className="gv-topline-wrapper">
       <div className='gv-topline-title'>{divInfo.glossTitle}</div>
-      { 
+      <div className='gv-topline-result'>
+      {divInfo.hasData ?
         sides.map((side, i) =>
           <div key={'sw' + i} className='gv-bar-wrap'>
             <div className='gv-side-label'>{side.key === 'Ayes' ? 'Yes' : 'No'}</div>
@@ -23,8 +25,9 @@ const Topline = ({ divInfo }) => {
               <div className='gv-num-label'>{side.key === 'Ayes' ? divInfo.ayesCount : divInfo.noesCount}</div>
             </div>
           </div>
-        )
+        ) : <div className='gv-placeholder-text'>Not voted yet</div>
       }
+      </div>
       <p className='gv-topline-description'>{divInfo.glossText}</p>
     </div>
   )
