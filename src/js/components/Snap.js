bf194dc8f55781291872a18e1fcecf5ccf6b96bd
Seán Clarke <sean.clarke@guardian.co.uk>
2018-12-07 10:42:46 +0000

    now with hatched fors in snap

--- src/js/components/Snap.js
+++ src/js/components/Snap.js
@@ -11,8 +11,10 @@ class Bar extends Component {
   render() {
 
     var percentwidth = `${this.props.width}%`
+    var camelparty = this.props.party.replace(/ /g, "");
 
-    return (<div className={`gv-bar gv-${(this.props.decision)} gv-${(this.props.party)}`} style={{width: percentwidth}}></div>)
+    return (<rect className={`gv-bar gv-${(this.props.decision)} gv-${(camelparty)} gv-hatch-${(this.props.hatch)}`} style={this.props.style} />
+    )
   }
 }
 
@@ -23,34 +25,83 @@ export default class Snap extends Component {
 
   render() {
 
-    const division = this.props.divisions;  
+    const division = this.props.divisions;
     var allvotes = division.ayesCount + division.noesCount;
-    
 
+    //var finishlinepercent = `${100 * (division.ayesCount / allvotes)}%`
     var ayesBars = [];
     var noesBars = [];
-    for (var prop in division.ayesByParty) {
-      ayesBars.push(<Bar key={'aye' + prop} decision={'for'} party={prop} count={division.ayesByParty[prop]} width={100 * (division.ayesByParty[prop] / allvotes)}/>)
-    } 
-    for (var prop in division.noesByParty) {
-      noesBars.push(<Bar key={'no' + prop} decision={'against'} party={prop} count={division.noesByParty[prop]} width={100 * (division.ayesByParty[prop] / allvotes)}/>)
-    } 
+    var hatchBars = [];
+
+    var ayesLeft = 100 * (division.noesCount / allvotes)
+
+    division.ayesByParty.forEach((p, i) => {
+      p.width = 100 * (p.votes / allvotes);
+      if (i == 0) {
+        p.left = ayesLeft;
+      } else {
+        p.left = division.ayesByParty[i - 1].left + division.ayesByParty[i - 1].width;
+      }
+      p.leftstring = `${p.left}%`;
+      p.widthstring = `${p.width}%`;
+      p.style = {
+        x: p.leftstring,
+        width: p.widthstring
+      }
+      ayesBars.push(<Bar key={'aye' + p.party} hatch={false} decision={'for'} party={p.party} count={p.votes} style={p.style} />)
+      hatchBars.push(<Bar key={'hatch' + p.party} hatch={true} decision={'for'} party={p.party} count={p.votes} style={p.style}/>)
+    })
+
+    division.noesByParty.forEach((p, i) => {
+      p.width = 100 * (p.votes / allvotes);
+      if (i == 0) {
+        p.left = 0;
+      } else {
+        p.left = division.noesByParty[i - 1].width + division.noesByParty[i - 1].left;
+      }
+      p.leftstring = `${p.left}%`;
+      p.widthstring = `${p.width}%`;
+      p.style = {
+        x: p.leftstring,
+        width: p.widthstring
+      }
+      noesBars.push(<Bar key={'aye' + p.party} hatch={false} decision={'for'} party={p.party} count={p.votes} style={p.style} />)
+    })
 
     return (
-<div className="gv-bbv-snap">
-<div className="gv-snap-title">How they voted</div>
-<div className="gv-snap-ayes">Ayes: {division.ayesCount}</div>
-<div className="gv-snap-noes">Noes: {division.noesCount}</div> 
-<div className="gv-bars">
+      <div className="gv-bbv-snap">
+        <a className='gv-snap__link' href="https://gu.com/p/a5f4a?CMP=snap">How your MP voted on the Brexit withdrawal deal</a>
 
-<div className="gv-ayesBars">{ayesBars}</div>
+        <div className="gv-snap-title">MPs defeated May's deal by XX votes</div>
+        <div className="gv-tranches">
+          <div className="gv-snap-noes gv-tranche"><div className="gv-label gv-against">Against</div><div className="gv-count">{division.ayesCount}</div></div>
+          <div className="gv-snap-ayes gv-tranche"><div className="gv-label gv-for">For</div><div className="gv-count">{division.noesCount}</div></div>
+        </div>
+
+        <div className="gv-bars">
+          <div className="gv-markers">
+            <div className="gv-halfway-line"></div>
+            <div className="gv-halfway-label">50%</div>
+
+          </div>
+          <svg className="gv-bars-svg">            <defs>
+            <pattern id="for-hatch" patternUnits="userSpaceOnUse" width="4" height="4">
+              <rect width="4" height="4" className='gv-for__rect'></rect>
+              <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" className='gv-for__path'></path>
+            </pattern>
+          </defs>{ayesBars}{hatchBars}{noesBars}</svg>
+
+        </div>
+        <div class="gv-sell">
+          <h3 className='gv-sell-label'>Full voting records for all MPs
+                        <div className='gv-sell__button'>
+              <svg className='mt-icon' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50"><path d="M38.65 25.58L37 27.21 26.79 37.44l-1.1-1.11 8.53-10.19H12.5v-2.28h21.72l-8.53-10.19 1.1-1.1L37 22.79l1.63 1.63v1.16z" /></svg>
+            </div>
+          </h3>
+        </div>
 
 
-
-<div></div>
-
-</div>
-</div>
-)
+      </div>
+    )
   }
 }
