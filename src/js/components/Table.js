a3fd987d707beb4633bc9dc253f0d4d8a1168a72
Antonio Voce <antonio.voce@guardian.co.uk>
2018-12-12 13:45:00 +0000

    Final changes

--- src/js/components/Table.js
+++ src/js/components/Table.js
@@ -3,6 +3,7 @@ import Search from './Search.js'
 import tracker from './../tracker'
 import Drawer from './Drawer'
 import { sortTable } from '../util'
+import { timingSafeEqual } from 'crypto';
 
 const parseMobileVote = (vote) => {
   if (!vote) {
@@ -24,10 +25,11 @@ export default class Table extends Component {
       super(props);
       this.state = {
         isMobile: window.innerWidth < 450,
+        isTablet: window.innerWidth < 740,
         filterText: '',
         sortConditions: {
           column: 'listAs',
-          isDesc: true
+          isAsc: false
         },
         expandedMps: []
       }
@@ -39,7 +41,7 @@ export default class Table extends Component {
     handleSort(column) {
       const sortConditions = {
         column,
-        isDesc: !this.state.sortConditions.isDesc
+        isAsc: !this.state.sortConditions.isAsc
       }
 
       this.setState({ sortConditions });
@@ -65,36 +67,42 @@ export default class Table extends Component {
       tracker('expandMPdetails','expandMPdetails')
     }
 
+    // renderArrow(isDesktop, column, isAsc, key) {
+    //   if (!isDesktop && column === key) {
+    //     return isAsc ? <img src='<%= path %>/assets/uparrow.png' className="gv-downtrg gv-downtrg--absolute" /> : <img src='<%= path %>/assets/downarrow.png' className="gv-downtrg" />
+    //   }
+    // }
+
 
     render() {
-      const { expandedMps, isMobile, sortConditions: { column, isDesc } } = this.state
+      const { expandedMps, isMobile, isTablet, sortConditions: { column, isAsc } } = this.state
       const membersInfo = this.props.members
         .filter(m => m.allText.indexOf(this.state.filterText.toLowerCase()) > -1)
-        .sort((a, b) => sortTable(a, b, column, isDesc))
+        .sort((a, b) => sortTable(a, b, column, isAsc))
 
       return(
         <div className="gv-outer-table">
         <Search filterText={this.state.filterText}   onFilterTextChange={this.handleFilterTextChange}/>
-          {isMobile ? <div className="gv-expand-disclaimer">Tap header to sort, tap rows to expand</div> : null}
+          <div className="gv-expand-disclaimer">Tap header to sort, tap rows to expand</div>
         <div className="int-table">
           <div className="int-row int-row--header">
-            <div className="int-cell" onClick={() => this.handleSort('party')}>{isMobile ? 'PTY' : 'Party'}</div>
-            <div className="int-cell" onClick={() => this.handleSort('listAs')}>Name</div>
-            <div className="int-cell" onClick={() => this.handleSort('constituency')}>{isMobile ? 'Const.' : 'Constituency'}</div>
+              <div className="int-cell" onClick={() => this.handleSort('party')}>{isMobile ? 'PTY' : 'Party'}</div>
+              <div className="int-cell" onClick={() => this.handleSort('listAs')}>Name</div>
+              <div className="int-cell" onClick={() => this.handleSort('constituency')}>{isMobile ? 'Const.' : 'Constituency'}</div>
               <div className="int-cell int-cell--vote" onClick={() => this.handleSort('vote')}>Main vote</div>
-              <div className="int-cell int-cell--reb" onClick={() => this.handleSort('isMainVoteRebel')}>{isMobile ? 'Rebel' : 'Rebel?'}</div>
+              <div className="int-cell int-cell--reb" onClick={() => this.handleSort('isMainVoteRebel')}>Rebel</div>
           </div>
           {
            membersInfo.map((member, i) => {
               const mainVote = member.votes.find(d => d.isMainVote)
-                const mainVoteString = isMobile ? parseMobileVote(mainVote.vote) : mainVote ? mainVote.vote === 'Did not vote' ? '––' : mainVote.vote : 'TBC'
+              const mainVoteString = isTablet ? parseMobileVote(mainVote.vote) : mainVote ? mainVote.vote === 'Did not vote' ? '––' : mainVote.vote : 'TBC'
               const shortParty = member.party
               const isOpen = expandedMps.indexOf(member.id) > -1
               return [
                 <div key={`member-row-${i}`} className="int-row int-row--mp" onClick={() => this.handleClick(member.id)}>
                   <div className={`int-cell int-cell--party int-color--${shortParty}`}>{shortParty}</div>
                   <div className="int-cell int-cell--name">{member.name} 
-                  {!isMobile ? isOpen ? <img src='<%= path %>/assets/uparrow.png' className="gv-downtrg" /> : <img src='<%= path %>/assets/downarrow.png' className="gv-downtrg" /> : null}
+                  {!isTablet ? isOpen ? <img src='<%= path %>/assets/uparrow.png' className="gv-downtrg" /> : <img src='<%= path %>/assets/downarrow.png' className="gv-downtrg" /> : null}
                   </div>
                   <div className="int-cell int-cell--const">{member.constituency}</div>
                   <div className={`int-cell int-cell--vote`}>{`${mainVoteString}${mainVote.teller ? '*' : ''}`}</div>
