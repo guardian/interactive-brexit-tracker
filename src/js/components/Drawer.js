ddb8c64c641a051b7a967962e0a41371da8d6d47
Antonio Voce <antonio.voce@guardian.co.uk>
2019-01-30 17:36:33 +0000

    Table v2

--- src/js/components/Drawer.js
+++ src/js/components/Drawer.js
@@ -3,10 +3,12 @@ import React from 'react'
 const Drawer = ({ member, isOpen, votes }) => 
   <div className="gv-drawer" >
     <div className="gv-vote-history-wrapper">
-    {<div>Brexit voting record</div>}
+    {/*<div>Brexit voting record</div>
     <div className="gv-vote-history-labels"><div className="gv-vote-history-label gv-pro-may">With Gvt.</div>
     <div className="gv-vote-history-label gv-against-may">Against Gvt.</div>
     </div>
+    */}
+    <div className='gv-vote-history-title'>Brexit voting record</div>
     <div className="gv-vote-history">
     {
       votes.map((d,i) => {
