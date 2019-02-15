e48ea25886788158b5fcfe907d0fa4dd6353eb89
Antonio Voce <antonio.voce@guardian.co.uk>
2019-02-15 13:23:54 +0000

    Add share buttons

--- src/render.js
+++ src/render.js
@@ -2,7 +2,14 @@ import mainHtml from "./src/templates/main.html!text"
 import rp from "request-promise"
 import Mustache from "mustache"
 
+const pageTitle = "How Brexit has created four new political factions"
+const pageUrl = "https://gu.com/p/ah786"
+
+const twitterLink = 'https://twitter.com/intent/tweet?text=' + encodeURI(pageTitle) + '&url=' + encodeURIComponent(pageUrl + '?CMP=share_btn_tw');
+const facebookLink = 'https://www.facebook.com/dialog/share?app_id=180444840287&href=' + encodeURIComponent(pageUrl + '?CMP=share_btn_fb');
+const emailLink = 'mailto:?subject=' + encodeURIComponent(pageTitle) + '&body=' + encodeURIComponent(pageUrl + '?CMP=share_btn_link');
+
 export async function render() {
     const sheet = await rp("https://interactive.guim.co.uk/docsdata-test/1Nsa-6GKRpvy4HRr9n3osXquZJOQQo9W2QztecVi4qPc.json");
-    return Mustache.render(mainHtml, JSON.parse(sheet).sheets); 
+    return Mustache.render(mainHtml, Object.assign({}, JSON.parse(sheet).sheets, {twitterLink, facebookLink, emailLink })); 
 } 
